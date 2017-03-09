8f5f7c9a22bf02742252fddcfbe47c7e9c4aa1b6
Daniela Sofrone <daniela.sofrone.sd@gmail.com>
2017-03-09 16:14:51 +0100

    Get LaunchRequest, YesIntent, seclectArticle to work

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ var Alexa = require("alexa-sdk");
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
+    alexa.appId = 'amzn1.ask.skill.e2befcfb-4f8a-4357-854e-1811f7f3c89c';
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit(':ask', 'Hallo Daniela, willkommen bei Outfittery! Wir haben gerade neue Frühlings- und Sommer - Outfits bekommen - möchtest du gleich eine Box bestellen?', 'Möchtest du eine neue Box bestellen?');
+        this.emit(':ask', 'Hallo Daniela! Willkommen bei Outfittery! Wir haben gerade neue Frühlings- und Sommer - Outfits bekommen - möchtest du gleich eine Box bestellen?', 'Möchtest du eine neue Box bestellen?');
     },
 
     'AMAZON.YesIntent' : function() {
-      this.emit( ':tell', 'Super. Ich brauche nur noch Details zu dem Inhalt deiner Box ')
-    },
-    'AMAZON.NoIntent' : function() {
-      this.emit( ':tell', 'Ok, dann vielleicht nächstes Mal. Ich wünsche die noch einen schönen Tag. ')
-    },
-    'selectArticle' : function() {
       this.emit( ':ask', 'Was genau brauchst du? Oberteile, Hose oder Schuhe? Oder möchtest du neue Outfits? ')
     },
+    'AMAZON.NoIntent' : function() {
+      this.emit( ':tell', 'Ok, dann vielleicht nächstes Mal. Ich wünsche dir noch einen schönen Tag. ')
+    },
+    'selectArticleIntent' : function() {
+      this.emit( ':ask', ' Dann werde ich sofort eine Box für dich packen lassen. Möchtest du die Box an die gleiche Adresse zustellen lassen?')
+    }
 
 };
-
-//ich werde sofort eine Box für dich packen lassen
